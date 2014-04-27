046a2f6d2b83883c5a81bd2c6c87b7b1eefb0c62
Master109 <gileadcosman@Gileads-MacBook-Pro.local>
2014-04-26 23:16:53 -0700

    Organized gameobjects with parents and almost done with lvl 1

---
+++ Eternal Legend/Assets/Untitled.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Untitled : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (GetComponent<SpriteRenderer>().color.a < .6 && GetComponent<SpriteRenderer> ().color.a > .4)
+		{
+			name = "GrassTile1 (Back 1)";
+			if (GetComponent<SpriteRenderer>().sortingOrder == 0)
+				GetComponent<SpriteRenderer>().sortingOrder = -32768;
+		}
+		else if (GetComponent<SpriteRenderer>().color.a < .3)
+		{
+			name = "GrassTile1 (Back 2)";
+			if (GetComponent<SpriteRenderer>().sortingOrder == 0)
+				GetComponent<SpriteRenderer>().sortingOrder = -32768;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
