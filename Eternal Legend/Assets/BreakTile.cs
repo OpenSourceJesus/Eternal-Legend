d84e7bee786a3b6b48d0432454aab9f006c0dc8c
Master109 <gileadcosman@Gileads-MacBook-Pro.local>
2014-05-03 20:15:58 -0700

    Added 2 more levels with breaking blocks

---
+++ Eternal Legend/Assets/BreakTile.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakTile : MonoBehaviour
+{
+	bool breaking;
+	public int breakTimer;
+	int initBreakTimer;
+
+	// Use this for initialization
+	void Start ()
+	{
+		initBreakTimer = breakTimer;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		if (breaking)
+		{
+			float a = GetComponent<SpriteRenderer>().color.a;
+			a -= .02f;
+			GetComponent<SpriteRenderer>().color = new Color(GetComponent<SpriteRenderer>().color.r, GetComponent<SpriteRenderer>().color.g, GetComponent<SpriteRenderer>().color.b, a);
+			breakTimer --;
+			if (breakTimer <= 0)
+				Destroy (gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D coll)
+	{
+		if (coll.collider.gameObject.name == "Player")
+		{
+			breaking = true;
+		}
+	}
+}
