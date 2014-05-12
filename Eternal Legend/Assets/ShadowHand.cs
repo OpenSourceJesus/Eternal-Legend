e53498841705f192e46bd0c78513ce9a9ad836c8
Master109 <gileadcosman@Gileads-MacBook-Pro.local>
2014-05-12 12:40:04 -0700

    Added another level and changed Shadow Hands

--- Eternal Legend/Assets/ShadowHand.cs
+++ Eternal Legend/Assets/ShadowHand.cs
@@ -3,50 +3,47 @@ using System.Collections;
 
 public class ShadowHand : MonoBehaviour
 {
-	public float moveSpeed = 1f;
-	public float dropSpeed = 1f;
-	bool dropping;
-	public float dropTriggerDist = 1f;
-	Vector2 initPlayerLoc;
-	public int minHeight = 1;
+	public Transform[] wayPoints;
+	int currentWayPoint = 0;
+	public int speed = 10;
+	public float slowRate = 4f;
+	public int visionRange = 25;
+	RaycastHit2D hit;
 
 	// Use this for initialization
 	void Start ()
 	{
-		if (dropTriggerDist == -1)
-			dropTriggerDist = transform.lossyScale.x;
+
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate ()
+	void Update ()
 	{
-		if (transform.position.y > GameObject.Find("Player").transform.position.y && Mathf.Abs(transform.position.x - GameObject.Find("Player").transform.position.x) <= dropTriggerDist)
+		hit = Physics2D.Raycast(transform.position, GameObject.Find("Player").transform.position - transform.position, visionRange);
+		if (hit.collider != null)
 		{
-			initPlayerLoc = GameObject.Find("Player").transform.position;
-			dropping = true;
-		}
-		if (!dropping)
-		{
-			Vector2 vel = GameObject.Find("ShadowHandTarget").transform.position - transform.position;
-			vel *= 9999999999;
-			vel = Vector2.ClampMagnitude(vel, moveSpeed);
-			if (minHeight != -1 && transform.position.y < GameObject.Find("ShadowHandTarget").transform.position.y && transform.position.y > GameObject.Find("Player").transform.position.y + minHeight)
-				vel.y = 0;
-			rigidbody2D.velocity = vel;
+			transform.Find("Vision").GetComponent<LineRenderer>().SetPosition(1, hit.point - (Vector2) transform.position);
+			if (hit.collider.gameObject.name == "Player")
+				Application.LoadLevel(Application.loadedLevel);
 		}
 		else
 		{
-			rigidbody2D.AddForce(new Vector2(0, -dropSpeed));
-			if (transform.position.y <= initPlayerLoc.y)
-				dropping = false;
+			//Quaternion dirToPlayer = Quaternion.FromToRotation(transform.position, GameObject.Find("Player").transform.position);
+			Vector2 toPlayer = GameObject.Find("Player").transform.position - transform.position;
+			toPlayer = Vector2.ClampMagnitude(toPlayer, visionRange);
+			transform.Find("Vision").GetComponent<LineRenderer>().SetPosition(1, toPlayer);
 		}
-	}
-	
-	void OnCollisionEnter2D (Collision2D coll)
-	{
-		if (coll.collider.gameObject.name == "Player")
+		if (wayPoints.Length == 0)
+			return;
+		Vector2 vel = wayPoints [currentWayPoint].position - transform.position;
+		vel *= slowRate;
+		vel = Vector2.ClampMagnitude(vel, speed);
+		rigidbody2D.velocity = vel;
+		if (Vector2.Distance(transform.position, wayPoints[currentWayPoint].position) < .1)
 		{
-			Application.LoadLevel(Application.loadedLevel);
+			currentWayPoint ++;
+			if (currentWayPoint == wayPoints.Length)
+				currentWayPoint = 0;
 		}
 	}
 }
